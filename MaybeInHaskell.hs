cdd3ecfead42bdd0cddf3d07f3c73c1404adda2d
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-02-08 00:53:39 +0900

    インデントスタイルを変更

--- MaybeInHaskell.hs
+++ MaybeInHaskell.hs
@@ -12,17 +12,16 @@ gcd' a b
       | otherwise = divide y (x `mod` y)
 
 main :: IO ()
-main =
-  do
-    -- Maybe自体を表示
-    print $ gcd' 12 18
-    print $ gcd' 12 (-18)
-    -- Maybeの中身の値を表示
-    putStrLn $ maybe "" show $ gcd' 12 18
-    putStrLn $ maybe "" show $ gcd' 12 (-18)
-    -- Maybeの中身の値を2乗して表示
-    print $ fmap (^ 2) $ gcd' 12 18
-    print $ fmap (^ 2) $ gcd' 12 (-18)
-    -- 関数fmapの代わりに演算子<$>を利用することもできる(アプリカティブスタイル)
-    print $ (^ 2) <$> gcd' 12 18
-    print $ (^ 2) <$> gcd' 12 (-18)
+main = do
+  -- Maybe自体を表示
+  print $ gcd' 12 18
+  print $ gcd' 12 (-18)
+  -- Maybeの中身の値を表示
+  putStrLn $ maybe "" show $ gcd' 12 18
+  putStrLn $ maybe "" show $ gcd' 12 (-18)
+  -- Maybeの中身の値を2乗して表示
+  print $ fmap (^ 2) $ gcd' 12 18
+  print $ fmap (^ 2) $ gcd' 12 (-18)
+  -- 関数fmapの代わりに演算子<$>を利用することもできる(アプリカティブスタイル)
+  print $ (^ 2) <$> gcd' 12 18
+  print $ (^ 2) <$> gcd' 12 (-18)
