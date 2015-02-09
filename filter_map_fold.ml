4e4ce72308acc1984d5a971eb5ff30fcb4aaffe4
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-02-10 02:50:47 +0900

    ガードを利用

--- filter_map_fold.ml
+++ filter_map_fold.ml
@@ -1,18 +1,16 @@
-let rec filter p = function
+let rec filter' p = function
   | [] -> []
-  | x :: xs ->
-    if p x
-      then x :: filter p xs
-      else filter p xs;;
+  | x :: xs when p x -> x :: filter' p xs
+  | _ :: xs -> filter' p xs;;
 
-let rec map f = function
+let rec map' f = function
   | [] -> []
-  | x :: xs -> f x :: map f xs;;
+  | x :: xs -> f x :: map' f xs;;
 
-let rec foldr f v = function
+let rec foldr' f v = function
   | [] -> v
-  | x :: xs -> f x (foldr f v xs);;
+  | x :: xs -> f x (foldr' f v xs);;
 
-let rec foldl f v = function
+let rec foldl' f v = function
   | [] -> v
-  | x :: xs -> foldl f (f v x) xs;;
+  | x :: xs -> foldl' f (f v x) xs;;
