425c99ec85644efa3734fb24750c02ca6f39761b
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-02-14 15:37:32 +0900

    不要なセミコロンを省略

--- filter_map_fold.ml
+++ filter_map_fold.ml
@@ -1,16 +1,16 @@
 let rec filter' p = function
   | [] -> []
   | x :: xs when p x -> x :: filter' p xs
-  | _ :: xs -> filter' p xs;;
+  | _ :: xs -> filter' p xs
 
 let rec map' f = function
   | [] -> []
-  | x :: xs -> f x :: map' f xs;;
+  | x :: xs -> f x :: map' f xs
 
 let rec foldr' f v = function
   | [] -> v
-  | x :: xs -> f x (foldr' f v xs);;
+  | x :: xs -> f x (foldr' f v xs)
 
 let rec foldl' f v = function
   | [] -> v
-  | x :: xs -> foldl' f (f v x) xs;;
+  | x :: xs -> foldl' f (f v x) xs
